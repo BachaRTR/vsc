2a7c7686fbcf6fa5d50969884b16c9ad90ded3f0
Ajay Rathore <ajayrtr@gmail.com>
2026-02-06 20:06:28 +0530

    Initial commit via script

---
+++ src/main/java/com/intertiaz6/vsc/VscApplication.java
@@ -0,0 +1,13 @@
+package com.intertiaz6.vsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intertiaz6/vsc/VscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intertiaz6.vsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
